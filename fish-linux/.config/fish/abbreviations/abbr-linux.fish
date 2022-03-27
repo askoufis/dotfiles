d71c9ee4bb9cc76889b346eee5e0264b30579b02
Adam Skoufis <adam.skoufis@gmail.com>
2022-03-27 23:14:02 +1100

    Add package management linux fish abbrs

--- fish-linux/.config/fish/abbreviations/abbr-linux.fish
+++ fish-linux/.config/fish/abbreviations/abbr-linux.fish
@@ -3,3 +3,9 @@ abbr -a -g eabbr-linux $EDITOR $HOME/.config/fish/abbreviations/abbr-linux.fish
 
 # --- Screenshot ---
 abbr -a -g ss "maim -s -u | xclip -selection clipboard -t image/png -i"
+
+# --- Package management ---
+# Requires pacman-contrib to be installed
+# Removes old packages from the pacman cache, keeping the last 3 versions
+abbr -a -g clear-package-cache "sudo paccache -rk3; paru -Sc --aur --noconfirm"
+abbr -a -g update-package-mirrors reflector --save /etc/pacman.d/mirrorlist -c AU -c NZ --latest 10 --sort rate --protocol https
