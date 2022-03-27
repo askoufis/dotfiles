17abf4a6e89aacedd203b96e6a21542ef60e7a4f
Adam Skoufis <adam.skoufis@gmail.com>
2022-03-27 23:13:45 +1100

    Rename `settings.lua` to `options.lua`

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,54 @@
+-- Searching
+-- show matching strings while performing a search
+vim.opt.incsearch = true
+-- case insensitive search
+vim.opt.ignorecase = true
+-- override ignorecase if the search pattern contains uppercase characters
+vim.opt.smartcase = true
+-- don't persist highlights after searching
+vim.opt.hlsearch = false
+
+-- Navigation
+-- scroll when the cursor is 8 lines from the top/vim.opt.tom of the screen
+vim.opt.scrolloff = 8
+
+-- Mouse
+-- Allow the mouse to be used
+vim.opt.mouse = "a"
+
+-- Colors
+-- enable 24-bit RGB colours in the TUI
+-- make colours appear correctly with tmux + alacritty
+-- https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6#vim-neovim
+vim.opt.termguicolors = true
+
+-- Interface
+-- show signs in the same spot as line numbers
+vim.opt.signcolumn = "yes"
+-- show line number
+vim.opt.number = true
+-- show line number relative to the current line
+vim.opt.relativenumber = true
+-- show and column number of the cursor
+vim.opt.ruler = true
+
+-- Text
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.softtabstop = 4
+-- vim.opt.cindent = true
+-- vim.opt.cinoptions = '(0'
+
+-- Security (apparently)
+vim.opt.modelines = 0
+
+-- Splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Netrw
+vim.g.netrw_liststyle = 3
+vim.g.netrw_banner = 0
