3a8d99a02ac7524e894b304e36d521bc1a9ea8ec
Adam Skoufis <adam.skoufis@gmail.com>
2022-03-31 22:31:46 +1100

    nvim: Clean up keymaps

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,5 +1,8 @@
--- bind to map for convenience
-local map = vim.api.nvim_set_keymap
+local function map(mode, lhs, rhs, opts)
+  opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+  vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+end
+
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
@@ -8,19 +11,16 @@ local map = vim.api.nvim_set_keymap
 --   term_mode = "t",
 --   command_mode = "c",
 
-local options = { noremap = true, silent = true }
-local loud_options = { noremap = true }
-
 -- unset the space key
-map('n', '<Space>', '', options)
+map('n', '<Space>', '')
 -- map leader to space
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Movement --
 -- moving up and down doesn't skip wrapped lines
-map('n', 'j', 'gj', options)
-map('n', 'k', 'gk', options)
+map('n', 'j', 'gj')
+map('n', 'k', 'gk')
 
 -- Overwrite <C-l> within netrw to it doesn't refresh the terminal
 -- https://github.com/christoomey/vim-tmux-navigator/issues/189#issuecomment-620485838
@@ -31,64 +31,65 @@ vim.cmd [[
   augroup END
 
   function! NetrwMapping()
-    nnoremap <silent> <buffer> <C-l> :TmuxNavigateRight<CR>
+    nnoremap <silent> <buffer> <C-l> :lua require'tmux'.move_right()<CR>
   endfunction
 ]]
+
 -- Resize with arrows
-map("n", "<C-Up>", ":resize -2<CR>", options)
-map("n", "<C-Down>", ":resize +2<CR>", options)
-map("n", "<C-Left>", ":vertical resize -2<CR>", options)
-map("n", "<C-Right>", ":vertical resize +2<CR>", options)
+map("n", "<C-Up>", ":resize -2<CR>")
+map("n", "<C-Down>", ":resize +2<CR>")
+map("n", "<C-Left>", ":vertical resize -2<CR>")
+map("n", "<C-Right>", ":vertical resize +2<CR>")
 
 -- Highlights
-map('n', '<leader>n', ':noh<CR>', options)
+map('n', '<leader>n', ':noh<CR>')
 
 -- Navigating uffers
-map('n', '<S-l>', ':bnext<CR>', options)
-map('n', '<S-h>', ':bprev<CR>', options)
+map('n', '<S-l>', ':bnext<CR>')
+map('n', '<S-h>', ':bprev<CR>')
 
 -- Visual
 -- Stay in indent mode
-map("v", "<", "<gv", options)
-map("v", ">", ">gv", options)
+map("v", "<", "<gv")
+map("v", ">", ">gv")
 
 -- Quickfix
-map('n', '<A-q>', ':cw<CR>', options)
-map('n', '<leader>j', ':cnext<CR>', options)
-map('n', '<leader>k', ':cprev<CR>', options)
+map('n', '<A-q>', ':cw<CR>')
+map('n', '<leader>j', ':cnext<CR>')
+map('n', '<leader>k', ':cprev<CR>')
 
 -- Editing --
 -- insert a new line below the current line without entering insert mode
 map('n', '<CR>', 'o<Esc>', {})
 -- remap f1 to escape so it doesn't open help
-map('i', '<F1>', '<Esc>', options)
+map('i', '<F1>', '<Esc>')
 -- globally replace the word under the cursor
-map('n', '<leader>*', ':%s/<c-r><c-w>//g<left><left>', options)
+map('n', '<leader>*', ':%s/<c-r><c-w>//g<left><left>')
 -- move text up and down
 -- Normal
-map('n', '<A-j>', ':m .+1<CR>==', options);
-map('n', '<A-k>', ':m .-2<CR>==', options);
+map('n', '<A-j>', ':m .+1<CR>==');
+map('n', '<A-k>', ':m .-2<CR>==');
 -- Visual
-map('v', '<A-j>', ':m .+1<CR>==', options);
-map('v', '<A-k>', ':m .-2<CR>==', options);
+map('v', '<A-j>', ':m .+1<CR>==');
+map('v', '<A-k>', ':m .-2<CR>==');
 -- Visual block
-map('x', '<A-j>', ":m '>+1<CR>gv-gv", options)
-map('x', '<A-k>', ":m '<-2<CR>gv-gv", options)
+map('x', '<A-j>', ":m '>+1<CR>gv-gv")
+map('x', '<A-k>', ":m '<-2<CR>gv-gv")
 
 -- Pasting
 -- Don't yank to buffer when pasting
-map('v', 'p', '"_dP', options)
+map('v', 'p', '"_dP')
 
 -- Other --
 -- Open explorer, aka netrw
-map('n', '<leader>x', ':Lex 30<CR>', options)
+map('n', '<leader>x', ':Lex 30<CR>')
 
 -- write to a file when you accidentally opened it without root privileges
-map('c', 'w!!', 'w !sudo tee % > /dev/null', options)
+map('c', 'w!!', 'w !sudo tee % > /dev/null')
 
 -- Glow
-map('n', '<leader>mp', ':Glow<CR>', options)
+map('n', '<leader>mp', ':Glow<CR>')
 
 -- Telescope
-map("n", "<C-p>", ":lua require'user.telescope'.project_files()<CR>", options)
-map("n", "<leader>g", ":Telescope live_grep<CR>", options)
+map("n", "<C-p>", ":lua require'user.telescope'.project_files()<CR>")
+map("n", "<leader>g", ":Telescope live_grep<CR>")
