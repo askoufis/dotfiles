156f5f401b93fd5a97ba51b1716013ec15a92795
askoufis <askoufis@seek.com.au>
2022-04-04 19:44:22 +1000

    [nvim]: Get lua formatting working again

--- nvim/.config/nvim/lua/user/indent-blankline.lua
+++ nvim/.config/nvim/lua/user/indent-blankline.lua
@@ -1,7 +1,7 @@
 local prequire = require('prequire')
 local indent_blankline = prequire('indent_blankline')
 
-vim.cmd [[highlight IndentBlanklineContextChar guifg=#626262 gui=nocombine]] -- MoonflyGrey241
+vim.cmd([[highlight IndentBlanklineContextChar guifg=#626262 gui=nocombine]]) -- MoonflyGrey241
 
 indent_blankline.setup {
   char = '▏',
--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -8,4 +8,14 @@ null_ls.setup {
     formatting.prettierd,
     formatting.stylua,
   },
+  on_attach = function(client)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+    end
+  end,
 }
