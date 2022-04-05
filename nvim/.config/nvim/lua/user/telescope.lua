063272f0f3aacd69e08be4672aacc610e8197143
askoufis <askoufis@seek.com.au>
2022-04-05 13:22:59 +1000

    [nvim]: Show file preview for telescope `project_files` function

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -5,7 +5,7 @@ local actions = require('telescope.actions')
 local M = {}
 
 M.project_files = function()
-  local opts = { previewer = false } -- define here if you want to define something
+  local opts = {} -- define here if you want to define something
   local ok = pcall(require('telescope.builtin').git_files, opts)
   if not ok then
     require('telescope.builtin').find_files(opts)
