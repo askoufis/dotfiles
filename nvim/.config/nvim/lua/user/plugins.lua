56198e4cf3379842de2e9093ff397cb70de72039
askoufis <askoufis@seek.com.au>
2022-04-08 14:25:41 +1000

    [nvim]: Remove diffview plugin

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -143,8 +143,6 @@ return packer.startup(function(use)
   -- Navigation
   use('ggandor/lightspeed.nvim')
 
-  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
