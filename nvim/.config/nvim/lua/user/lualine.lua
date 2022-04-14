4f2bf6756c8be2a0653cf8fc440a28f27e0561b1
Adam Skoufis <adam.skoufis@gmail.com>
2022-04-14 23:39:05 +1000

    [nvim]: Show relative path in lualine

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -16,7 +16,7 @@ prequire('lualine').setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', path = 1 } },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' },
@@ -24,7 +24,7 @@ prequire('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { 'filename' },
+    lualine_c = { { 'filename', path = 1 } },
     lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {},
