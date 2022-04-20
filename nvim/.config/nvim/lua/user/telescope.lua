f189d477127fe24247169a15ec84fdb5677b6b1e
askoufis <askoufis@seek.com.au>
2022-04-20 17:12:41 +1000

    [nvim]: Explicitly include `.config` in gitignore so `rg` searches it

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -3,7 +3,7 @@ local telescope = prequire('telescope')
 local actions = require('telescope.actions')
 
 local project_files = function()
-  local opts = {} -- define here if you want to define something
+  local opts = { hidden = true }
   local ok = pcall(require('telescope.builtin').git_files, opts)
   if not ok then
     require('telescope.builtin').find_files(opts)
@@ -16,24 +16,11 @@ local function map(mode, l, r, opts)
 end
 
 map('n', '<C-p>', project_files)
-map('n', '<leader>g', ':Telescope live_grep<CR>')
+map('n', '<leader>g', require('telescope.builtin').live_grep)
 
 telescope.setup {
   defaults = {
-    file_ignore_patterns = { 'node_modules' },
     path_display = { 'smart' },
-
-    -- Once stow supports the dotfiles flag then this can be removed
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '-uu', -- ignore gitignore and search hidden files/folders
-    },
 
     mappings = {
       i = {
@@ -105,26 +92,6 @@ telescope.setup {
     find_file = {
       hidden = true,
     },
-    live_grep = {
-      hidden = true,
-    },
-    git_files = {
-      hidden = true,
-    },
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-  },
-  extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
   },
 }
 
