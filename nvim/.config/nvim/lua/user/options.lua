c2f7967cd5f0af2f4a724a4ec6f68a5259b96aad
askoufis <askoufis@seek.com.au>
2022-05-18 11:08:50 +1000

    [nvim]: Correctly disable auto-wrap in markdown files

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -48,9 +48,16 @@ vim.opt.linebreak = true
 vim.opt.textwidth = 100
 
 -- Formatting
--- Don't wrap text using textwidth
--- See :help fo-table
-vim.opt.formatoptions:remove { 't' }
+-- Don't wrap text using textwidth in markdown files
+local format_options = vim.api.nvim_create_augroup('format_options', {})
+vim.api.nvim_create_autocmd('Filetype', {
+  pattern = 'markdown',
+  group = format_options,
+  desc = 'Disable auto-wrap in markdown files',
+  callback = function()
+    vim.opt.formatoptions:remove { 't' }
+  end,
+})
 
 -- Security (apparently)
 vim.opt.modelines = 0
