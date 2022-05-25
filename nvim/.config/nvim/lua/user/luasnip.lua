9248452e21df3f451865479b920abf9a945a0000
askoufis <askoufis@seek.com.au>
2022-05-25 17:34:35 +1000

    [nvim]: Tweak luasnip config

--- nvim/.config/nvim/lua/user/luasnip.lua
+++ nvim/.config/nvim/lua/user/luasnip.lua
@@ -1,3 +1,9 @@
 local prequire = require('prequire')
+local ls = prequire('luasnip')
+
+ls.config.set_config {
+  history = true,
+  updateevents = 'TextChanged,TextChangedI',
+}
 
 prequire('luasnip.loaders.from_lua').load { paths = './lua/user/snippets' }
