847ac509ba87b73f2ec5fc222bba181f91cc2f92
Adam Skoufis <adam.skoufis@gmail.com>
2022-10-02 19:07:25 +1100

    [nvim]: Set the `MsgArea` highlight to something nicer

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -6,3 +6,6 @@ vim.cmd([[
     set background=dark
   endtry
 ]])
+
+-- Set the `MsgArea` highlight to the values of the normal group so it looks nicer
+vim.api.nvim_set_hl(0, 'MsgArea', { fg = 'fg', bg = 'bg' })
