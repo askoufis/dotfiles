2f0c2559fa4d79d11f7142a29b08114f7f65f985
askoufis <askoufis@seek.com.au>
2022-10-20 17:15:54 +1100

    [fish-mac]: Remove `bun` config

--- fish-mac/.config/fish/environment/env-mac.fish
+++ fish-mac/.config/fish/environment/env-mac.fish
@@ -1,5 +1,1 @@
 set -x -g STOW_FOLDERS alacritty alacritty-mac nvim fish fish-mac tmux tmuxinator
-
-# Bun
-set -Ux BUN_INSTALL "/Users/askoufis/.bun"
-set -px --path PATH "/Users/askoufis/.bun/bin"
