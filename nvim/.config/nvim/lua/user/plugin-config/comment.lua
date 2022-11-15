6e1e56be9658c8af2ef35bf8abaa17fba439b1df
Adam Skoufis <adam.skoufis@gmail.com>
2022-11-15 23:17:15 +1100

    [nvim]: Use pre-made `Comment.nvim` pre-hook to fix treesitter context commentstring

--- nvim/.config/nvim/lua/user/plugin-config/comment.lua
+++ nvim/.config/nvim/lua/user/plugin-config/comment.lua
@@ -1,27 +1,5 @@
 local comment = require('Comment')
 
 comment.setup {
-  pre_hook = function(ctx)
-    -- Only calculate commentstring for tsx filetypes
-    if vim.bo.filetype == 'typescriptreact' then
-      local U = require('Comment.utils')
-
-      -- Determine the location where to calculate commentstring from
-      local location = nil
-
-      if ctx.ctype == U.ctype.block then
-        location = require('ts_context_commentstring.utils').get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require('ts_context_commentstring.utils').get_visual_start_location()
-      end
-
-      -- Detemine whether to use linewise or blockwise commentstring
-      local type = ctx.ctype == U.ctype.line and '__default' or '__multiline'
-
-      return require('ts_context_commentstring.internal').calculate_commentstring {
-        key = type,
-        location = location,
-      }
-    end
-  end,
+  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 }
