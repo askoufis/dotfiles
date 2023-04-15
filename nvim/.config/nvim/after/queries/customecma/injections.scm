377c95d5a95cd5dad1a9d41f46b8255bde31d5c4
Adam Skoufis <askoufis@seek.com.au>
2023-04-15 19:58:56 +1000

    [nvim]: Add custom injections for highlighting template strings prefixed with `ts` and `tsx` comments
    - Prefixing a template string with a /* ts */ or /* tsx */ will
      highlight the text inside string as if it were the appropriate
      language. This mimics the feature from VSCode.

---
+++ nvim/.config/nvim/after/queries/customecma/injections.scm
@@ -0,0 +1,18 @@
+; Generic injections for use in all ecmascript filetypes
+; I think calling this folder `ecma` caused issues with inheritance
+; because nvim-treesitter contains its own `ecma` injections
+; and I guess you can't inherit from both at once or something
+
+; Also seems like you can't have query folder names with capitals
+; or hyphens in them
+
+; Modified from https://github.com/nvim-treesitter/nvim-treesitter/blob/f499a711fa5c573b5ba0af09a3c9955a6c14b062/queries/ecma/injections.scm#L81
+(
+  (comment) @_ts_comment (#eq? @_ts_comment "/* ts */")
+  (template_string) @typescript (#offset! @typescript 0 1 0 -1)
+)
+
+(
+  (comment) @_tsx_comment (#eq? @_tsx_comment "/* tsx */")
+  (template_string) @tsx (#offset! @tsx 0 1 0 -1)
+)
