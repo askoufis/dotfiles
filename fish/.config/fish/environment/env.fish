7bcea1500c3ebfe46a6fd7f063a3586ccbb3aa45
Adam Skoufis <adam.skoufis@gmail.com>
2023-05-06 20:50:54 +1000

    [fish]: Add `.local/bin` to path

--- fish/.config/fish/environment/env.fish
+++ fish/.config/fish/environment/env.fish
@@ -19,6 +19,7 @@ set -x -g VOLTA_HOME $HOME/.local/share/.volta
 fish_add_path -pP $VOLTA_HOME/bin
 
 fish_add_path -pP ~/.bun/bin
+fish_add_path -pP ~/.local/bin
 
 # Set `bat` as the `man` pager
 # https://github.com/sharkdp/bat/#man
