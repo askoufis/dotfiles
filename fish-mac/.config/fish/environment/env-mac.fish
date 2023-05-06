3c0e48aa4b99aed0d192e79a47457884d03f702b
Adam Skoufis <askoufis@seek.com.au>
2023-05-06 23:36:50 +1000

    [fish]: Add `git` to mac `STOW_FOLDERS`

--- fish-mac/.config/fish/environment/env-mac.fish
+++ fish-mac/.config/fish/environment/env-mac.fish
@@ -1,2 +1,2 @@
-set -x -g STOW_FOLDERS alacritty alacritty-mac nvim fish fish-mac tmux tmuxinator
+set -x -g STOW_FOLDERS alacritty alacritty-mac git nvim fish fish-mac tmux tmuxinator
 eval (/opt/homebrew/bin/brew shellenv)
