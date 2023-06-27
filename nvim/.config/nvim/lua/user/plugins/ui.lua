12e1eb2bf0ae29e42c570cbb2408e1e3a98c673b
Adam Skoufis <askoufis@seek.com.au>
2023-06-28 09:28:30 +1000

    [nvim]: Pin `fidget.nvim` to `legacy` tag
    - Plugin is being rewritten. See https://github.com/j-hui/fidget.nvim/tree/f1c375ba68839eaa4a65efdf2aa078c0da0548fe#fidgetnvim

--- nvim/.config/nvim/lua/user/plugins/ui.lua
+++ nvim/.config/nvim/lua/user/plugins/ui.lua
@@ -9,5 +9,7 @@ return {
   { 'nvim-lua/popup.nvim', lazy = true },
 
   -- LSP progress inidicator
-  { 'j-hui/fidget.nvim', event = 'BufAdd', config = true },
+  -- Plugin is being re-written, pinned to `legacy` tag to avoid breaking changes
+  -- See https://github.com/j-hui/fidget.nvim/tree/f1c375ba68839eaa4a65efdf2aa078c0da0548fe#fidgetnvim
+  { 'j-hui/fidget.nvim', tag = 'legacy', event = 'BufAdd', config = true },
 }
