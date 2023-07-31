1732b069c3a92687831ab5989de986055fe8e3e3
Adam Skoufis <adam.skoufis@gmail.com>
2023-07-31 23:10:34 +1000

    [nvim]: Simplify `rust_analyzer` lsp configuration

--- nvim/.config/nvim/lua/user/plugins/lsp/servers/rust_analyzer.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/servers/rust_analyzer.lua
@@ -1,10 +1,6 @@
 local lspconfig = require('lspconfig')
 local common_on_attach = require('user.plugins.lsp.handlers').on_attach
 
-local on_attach = function(client, bufnr)
-  common_on_attach { disable_formatting = false }(client, bufnr)
-end
-
 lspconfig.rust_analyzer.setup {
-  on_attach = on_attach,
+  on_attach = common_on_attach { disable_formatting = false },
 }
