4f9f49abdfa39fcf5beaf91ec4ace4bd91f31100
Adam Skoufis <askoufis@seek.com.au>
2023-09-01 15:51:07 +1000

    [fish]: Set `VOLTA_FEATURE_PNPM` envar to enable experimental volta PNPM support

--- fish/.config/fish/environment/env.fish
+++ fish/.config/fish/environment/env.fish
@@ -18,6 +18,8 @@ set fish_greeting
 set -x -g VOLTA_HOME $HOME/.local/share/.volta
 fish_add_path -pP $VOLTA_HOME/bin
 
+set -x -g VOLTA_FEATURE_PNPM 1
+
 fish_add_path -pP ~/.bun/bin
 fish_add_path -pP ~/.local/bin
 
