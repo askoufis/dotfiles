ac0250c8afed9d2819d9756d25c23c3157b7693a
Adam Skoufis <adam.skoufis@gmail.com>
2023-10-15 12:19:35 +1100

    [nvim]: Update `indent-blankline` config to be compatible with v3
    - https://github.com/lukas-reineke/indent-blankline.nvim/wiki/Migrate-to-version-3

--- nvim/.config/nvim/lua/user/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/user/plugins/indent-blankline.lua
@@ -1,16 +1,16 @@
 return {
-  -- Indent highlighting
   'lukas-reineke/indent-blankline.nvim',
   version = '*',
   event = { 'BufReadPost', 'BufNewFile' },
-  config = function(_, opts)
-    require('indent_blankline').setup(opts)
-    vim.api.nvim_set_hl(0, 'IndentBlanklineContextChar', { link = 'MoonflyGrey241' })
-  end,
+  main = 'ibl',
   opts = {
-    char = '▏',
-    context_char = '▏',
-    show_current_context = true,
-    show_current_context_start = false,
+    debounce = 30,
+    indent = {
+      char = '▏',
+    },
+    scope = {
+      show_start = false,
+      highlight = { 'MoonflyGrey241' },
+    },
   },
 }
