dd75f4317ff749db963abfda9494c40ea80d78a0
Adam Skoufis <askoufis@seek.com.au>
2023-10-16 15:18:38 +1100

    [fish]: Remove `-l` flags from `set` commands in `gwj` function

--- fish/.config/fish/functions/gwj.fish
+++ fish/.config/fish/functions/gwj.fish
@@ -1,6 +1,6 @@
 function gwj
-  set -l query $argv[1]
-  set -l out (
+  set query $argv[1]
+  set out (
     git worktree list |
     # Pass the query to fzf, auto-accept if there's only one option
     fzf --preview='git log --oneline -n10 {2}' --query "$query" -1 |
