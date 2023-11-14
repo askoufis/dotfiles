962db6bcd0ebc0655400f8339a4fed43936cbc42
Adam Skoufis <askoufis@seek.com.au>
2023-11-14 11:56:05 +1100

    [nvim]: Add `nvim-lsp-file-operations` plugin

--- nvim/.config/nvim/lua/user/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/init.lua
@@ -95,4 +95,14 @@ return {
       }
     end,
   },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim',
+    },
+    config = function()
+      require('lsp-file-operations').setup()
+    end,
+  },
 }
