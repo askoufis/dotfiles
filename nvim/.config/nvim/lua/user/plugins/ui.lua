c2ebe70b829d3ba4e99bf3683dd218ffc7329ff7
Adam Skoufis <adam.skoufis@gmail.com>
2023-12-17 20:50:49 +1100

    [nvim]: Unpin `fidget.nvim` plugin version now that the rewrite is complete

--- nvim/.config/nvim/lua/user/plugins/ui.lua
+++ nvim/.config/nvim/lua/user/plugins/ui.lua
@@ -9,7 +9,5 @@ return {
   { 'nvim-lua/popup.nvim', lazy = true },
 
   -- LSP progress inidicator
-  -- Plugin is being re-written, pinned to `legacy` tag to avoid breaking changes
-  -- See https://github.com/j-hui/fidget.nvim/tree/f1c375ba68839eaa4a65efdf2aa078c0da0548fe#fidgetnvim
-  { 'j-hui/fidget.nvim', tag = 'legacy', event = 'BufAdd', config = true },
+  { 'j-hui/fidget.nvim', version = '*', event = 'BufAdd', config = true },
 }
