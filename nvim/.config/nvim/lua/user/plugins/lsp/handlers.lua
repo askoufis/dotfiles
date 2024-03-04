ba2d0d1c81ddee24e8971ea147bac4723e82a216
Adam Skoufis <adam.skoufis@gmail.com>
2024-03-04 22:41:13 +1100

    [nvim]: Remove old format-on-save autocommand

--- nvim/.config/nvim/lua/user/plugins/lsp/handlers.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/handlers.lua
@@ -106,15 +106,6 @@ M.on_attach = function(options)
       map('n', '<leader>f', function()
         vim.lsp.buf.format { async = true }
       end)
-
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = bufnr,
-        desc = 'Format the buffer on save',
-        group = vim.api.nvim_create_augroup('lsp_formatting_buf_' .. bufnr, {}),
-        callback = function()
-          vim.lsp.buf.format()
-        end,
-      })
     end
   end
 end
