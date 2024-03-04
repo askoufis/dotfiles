8925b3bcd0b1a5f15592e380e7d23988195f4fc9
Adam Skoufis <adam.skoufis@gmail.com>
2024-03-04 22:42:07 +1100

    [nvim]: Tweak Lua lsp settings
    - https://github.com/nvim-lua/kickstart.nvim/blob/c9122e89e3c2e19cd784642a1d0cddae258d0672/init.lua#L551-L570

--- nvim/.config/nvim/lua/user/plugins/lsp/servers/lua_ls.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/servers/lua_ls.lua
@@ -1,7 +1,3 @@
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
 local lspconfig = require('lspconfig')
 local common_on_attach = require('user.plugins.lsp.handlers').on_attach
 
@@ -12,19 +8,21 @@ lspconfig.lua_ls.setup {
       runtime = {
         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
         version = 'LuaJIT',
-        -- Setup your lua path
-        path = runtime_path,
-      },
-      diagnostics = {
-        globals = { 'vim' },
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
+        library = {
+          '${3rd}/luv/library',
+          unpack(vim.api.nvim_get_runtime_file('', true)),
+        },
         -- Don't really care that much about having a setup that matches my runtime
         -- since I'm not doing any serious lua dev at the moment
         -- https://github.com/LuaLS/lua-language-server/wiki/Settings#workspacecheckthirdparty
         checkThirdParty = false,
+      },
+      -- Enable Lua LSP's built-in snippets
+      completion = {
+        callSnippet = 'Replace',
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
