0cfa4c1f0f757b4c0d521e216eec9280d604334c
Adam Skoufis <askoufis@seek.com.au>
2024-03-05 15:57:49 +1100

    [nvim]: Add postfix snippets for curly quotes, fix `apos` snippet to use the correct quote

--- nvim/.config/nvim/lua/user/snippets/all.lua
+++ nvim/.config/nvim/lua/user/snippets/all.lua
@@ -1,4 +1,43 @@
 return {
   -- Do this instead of &apos;
-  s('apos', t('‘')),
+  s('apos', t('’')),
+
+  postfix({
+    trig = '.lsquo',
+    desc = 'Convert the previous apostraphe into a opening single quote, or insert if missing',
+    match_pattern = "'$",
+  }, {
+    t('‘'),
+  }),
+
+  postfix({
+    trig = '.apos',
+    desc = 'Convert the previous apostraphe into an closing single quote, or insert if missing',
+    match_pattern = "'$",
+  }, {
+    t('’'),
+  }),
+  postfix({
+    trig = '.rsquo',
+    desc = 'Convert the previous apostraphe into closing single quote, or insert if missing',
+    match_pattern = "'$",
+  }, {
+    t('’'),
+  }),
+
+  postfix({
+    trig = '.ldquo',
+    desc = 'Convert the previous apostraphe into an opening double quote, or insert if missing',
+    match_pattern = '"$',
+  }, {
+    t('“'),
+  }),
+
+  postfix({
+    trig = '.rdquo',
+    desc = 'Convert the previous apostraphe into a closing double quote, or insert if missing',
+    match_pattern = '"$',
+  }, {
+    t('”'),
+  }),
 }
