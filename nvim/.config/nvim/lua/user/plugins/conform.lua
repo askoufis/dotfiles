aeceff9eb336e7e7c1d9cb4f49da8b0bbc2f23a2
Adam Skoufis <askoufis@seek.com.au>
2024-07-31 16:59:46 +1000

    [nvim]: Update `conform.nvim` configuration
    - Some config was recently deprecated. See https://github.com/stevearc/conform.nvim/pull/491.

--- nvim/.config/nvim/lua/user/plugins/conform.lua
+++ nvim/.config/nvim/lua/user/plugins/conform.lua
@@ -1,4 +1,4 @@
-local prettierd = { 'prettierd' }
+local prettierd = { 'prettierd', 'prettier', stop_after_first = true }
 
 return {
   'stevearc/conform.nvim',
@@ -14,27 +14,25 @@ return {
       desc = 'Format buffer',
     },
   },
-  -- Everything in opts will be passed to setup()
   opts = {
-    -- Define your formatters
     formatters_by_ft = {
       lua = { 'stylua' },
-      css = { prettierd },
-      graphql = { prettierd },
-      html = { prettierd },
-      javascript = { prettierd },
-      javasriptreact = { prettierd },
-      json = { prettierd },
-      jsonc = { prettierd },
-      json5 = { prettierd },
-      less = { prettierd },
-      markdown = { prettierd },
-      mdx = { prettierd },
-      typescript = { prettierd },
-      typescriptreact = { prettierd },
-      yaml = { prettierd },
+      css = prettierd,
+      graphql = prettierd,
+      html = prettierd,
+      javascript = prettierd,
+      javasriptreact = prettierd,
+      json = prettierd,
+      jsonc = prettierd,
+      json5 = prettierd,
+      less = prettierd,
+      markdown = prettierd,
+      mdx = prettierd,
+      typescript = prettierd,
+      typescriptreact = prettierd,
+      yaml = prettierd,
     },
-    -- Set up format-on-save
-    format_on_save = { notify_on_error = false, timeout_ms = 500, lsp_fallback = true },
+    format_on_save = { timeout_ms = 500, lsp_format = 'fallback' },
+    notify_on_error = false,
   },
 }
