722775ed5ec217f80628ecdc65914823725a9f4a
Adam Skoufis <askoufis@seek.com.au>
2024-08-17 23:42:35 +1000

    [fish-linux, fish-mac]: Enable OS-specific `ghostty` stow folders

--- fish-linux/.config/fish/environment/env-linux.fish
+++ fish-linux/.config/fish/environment/env-linux.fish
@@ -1,1 +1,1 @@
-set -x -g STOW_FOLDERS alacritty alacritty-linux ghostty git nvim fish fish-linux tmux
+set -x -g STOW_FOLDERS alacritty alacritty-linux ghostty ghostty-linux git nvim fish fish-linux tmux
