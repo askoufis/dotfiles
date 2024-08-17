855aeaecf4885eff00e5d49f13ccf4efd1baa5f1
Adam Skoufis <adam.skoufis@gmail.com>
2024-08-17 23:00:30 +1000

    [fish-linux, fish-mac]: Enable stowing of `ghostty` config directory

--- fish-linux/.config/fish/environment/env-linux.fish
+++ fish-linux/.config/fish/environment/env-linux.fish
@@ -1,1 +1,1 @@
-set -x -g STOW_FOLDERS alacritty alacritty-linux git nvim fish fish-linux tmux
+set -x -g STOW_FOLDERS alacritty alacritty-linux ghostty git nvim fish fish-linux tmux
--- fish-mac/.config/fish/environment/env-mac.fish
+++ fish-mac/.config/fish/environment/env-mac.fish
@@ -1,4 +1,4 @@
-set -x -g STOW_FOLDERS alacritty alacritty-mac git nvim fish fish-mac tmux tmuxinator
+set -x -g STOW_FOLDERS alacritty alacritty-mac ghostty git nvim fish fish-mac tmux tmuxinator
 eval (/opt/homebrew/bin/brew shellenv)
 
 # Something is overriding $SHELL to be `/bin/sh`, so force it to be fish instead so tmux starts with fish
