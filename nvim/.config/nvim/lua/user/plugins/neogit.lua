dde86d319f196a22ead931e0f4f58739fdc68f12
Adam Skoufis <adam.skoufis@gmail.com>
2024-08-18 13:49:36 +1000

    [nvim]: Always start in normal mode when committing with `neogit`

--- nvim/.config/nvim/lua/user/plugins/neogit.lua
+++ nvim/.config/nvim/lua/user/plugins/neogit.lua
@@ -2,6 +2,10 @@ return {
   'TimUntersberger/neogit',
   dependencies = 'nvim-lua/plenary.nvim',
   cmd = 'Neogit',
+  opts = {
+    -- Always start in normal mode when committing
+    disable_insert_on_commit = true,
+  },
   keys = {
     { '<leader>G', ':Neogit<CR>', desc = 'Toggle Neogit', silent = true },
   },
