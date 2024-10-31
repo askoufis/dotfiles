4b538cdbab2a5bcc24826b2021f71dd266f37bfb
Adam Skoufis <askoufis@seek.com.au>
2024-10-31 13:48:55 +1100

    [fish-mac]: Increase maximum open file limit

--- fish-mac/.config/fish/environment/env-mac.fish
+++ fish-mac/.config/fish/environment/env-mac.fish
@@ -4,3 +4,7 @@ eval (/opt/homebrew/bin/brew shellenv)
 # Something is overriding $SHELL to be `/bin/sh`, so force it to be fish instead so tmux starts with fish
 set -x -g SHELL /opt/homebrew/bin/fish
 
+# Increase the maximum number of open files and processes
+# https://gist.github.com/tombigel/d503800a282fcadbee14b537735d202c
+ulimit -n 200000
+ulimit -u 2048
