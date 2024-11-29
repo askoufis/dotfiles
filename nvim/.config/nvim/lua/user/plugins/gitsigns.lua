741453a980daf7ae119987c864f9ba081d581009
Adam Skoufis <askoufis@seek.com.au>
2024-11-29 11:57:04 +1100

    [nvim]: Remove default `gitsigns.nvim` config

--- nvim/.config/nvim/lua/user/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/user/plugins/gitsigns.lua
@@ -5,6 +5,7 @@ return {
   config = true,
   opts = function()
     local gitsigns = require('gitsigns')
+
     return {
       signs = {
         add = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
@@ -18,40 +19,7 @@ return {
           linehl = 'GitSignsChangeLn',
         },
       },
-      signcolumn = true,
-      numhl = false,
-      linehl = false,
-      word_diff = false,
-      watch_gitdir = {
-        interval = 1000,
-        follow_files = true,
-      },
       attach_to_untracked = true,
-      current_line_blame = false,
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
-        ignore_whitespace = false,
-      },
-      current_line_blame_formatter_opts = {
-        relative_time = false,
-      },
-      sign_priority = 6,
-      update_debounce = 100,
-      status_formatter = nil, -- Use default
-      max_file_length = 40000,
-      preview_config = {
-        -- Options passed to nvim_open_win
-        border = 'single',
-        style = 'minimal',
-        relative = 'cursor',
-        row = 0,
-        col = 1,
-      },
-      yadm = {
-        enable = false,
-      },
       on_attach = function(bufnr)
         local map = function(mode, l, r, opts)
           opts = opts or { silent = true }
