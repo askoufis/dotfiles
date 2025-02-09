e516e590019e7d4a73609462d9a4ced1b0fc4131
Adam Skoufis <askoufis@seek.com.au>
2025-02-10 09:54:47 +1100

    [nvim]: Update `html` LSP config to enable completions

--- nvim/.config/nvim/lua/user/plugins/lsp/servers/html.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/servers/html.lua
@@ -1,6 +1,12 @@
 local lspconfig = require('lspconfig')
 local common_on_attach = require('user.plugins.lsp.handlers').on_attach
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- Enables element and attribute competions
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 lspconfig.html.setup {
+  capabilities = capabilities,
+  -- Let prettierd handle HTML formatting
   on_attach = common_on_attach { disable_formatting = true },
 }
