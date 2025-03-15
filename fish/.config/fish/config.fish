13197cd9fc3c4d4b4b4376076d64fa4d11dfc79c
Adam Skoufis <adam.skoufis@gmail.com>
2025-03-15 17:07:22 +1100

    [fish]: Simplify config loading, change how private config is loaded

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,13 +5,9 @@ if status --is-interactive
     # stop Ctrl-s and Ctrl-q from screwing stuff up
     stty -ixon # Commands to run in interactive sessions can go here
 
-    # source private tokens and keys
-    set -l PRIVATE_FISH ~/.config/fish/private.fish
-    if test -e $PRIVATE_FISH
-        source $PRIVATE_FISH
-        if test $status -ne 0
-            echo "Failed to source private keys from $PRIVATE_FISH"
-        end
+    # source the contents of the `private` directory
+    for FILE in $HOME/.config/fish/private/*
+        source $FILE
     end
 
     # Source env first as other files depend on envars
@@ -19,12 +15,9 @@ if status --is-interactive
     # Source base `env.fish` before OS-specific env files
     source $HOME/.config/fish/environment/env.fish
 
-    if test -e $HOME/.config/fish/environment/env-mac.fish
-        source $HOME/.config/fish/environment/env-mac.fish
-    end
-
-    if test -e $HOME/.config/fish/environment/env-linux.fish
-        source $HOME/.config/fish/environment/env-linux.fish
+    # OS-specific env files
+    for FILE in $HOME/.config/fish/environment/env-*
+        source $FILE
     end
 
     # --- Abbreviations
