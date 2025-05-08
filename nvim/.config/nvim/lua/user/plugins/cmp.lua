85e834597c95f614cd69a24f61644fa37c620c5f
Adam Skoufis <adam.skoufis@gmail.com>
2025-05-08 20:49:59 +1000

    [nvim]: Replace `nvim-cmp` with `blink.cmp`

--- nvim/.config/nvim/lua/user/plugins/cmp.lua
+++ nvim/.config/nvim/lua/user/plugins/cmp.lua
@@ -1,171 +1,21 @@
-local disable_cmp_in = { TelescopePrompt = true }
-
-local kind_icons = {
-  Text = '󰊄',
-  Method = 'm',
-  Function = '󰊕',
-  Constructor = '',
-  Field = '',
-  Variable = '',
-  Class = '󰌗',
-  Interface = '',
-  Module = '',
-  Property = '',
-  Unit = '',
-  Value = 'v',
-  Enum = '',
-  Keyword = '',
-  Snippet = '',
-  Color = '',
-  File = '',
-  Reference = '',
-  Folder = '',
-  EnumMember = '',
-  Constant = '',
-  Struct = '',
-  Event = '',
-  Operator = '',
-  TypeParameter = '󰉺',
-}
-
 return {
-  'hrsh7th/nvim-cmp',
-  event = 'InsertEnter',
-  version = false, -- last release is way too old
-  dependencies = {
-    'hrsh7th/cmp-buffer', -- buffer completions
-    'hrsh7th/cmp-path', -- path completions
-    'hrsh7th/cmp-cmdline', -- cmdline completions
-    'saadparwaiz1/cmp_luasnip', -- snippet completions
-    'hrsh7th/cmp-nvim-lsp', -- LSP completions
-    'hrsh7th/cmp-nvim-lua', -- nvim lua config completions
+  'saghen/blink.cmp',
+  version = '1.*',
+  dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = { preset = 'enter' },
+    appearance = {
+      nerd_font_variant = 'mono',
+    },
+    completion = { documentation = { auto_show = true } },
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
+    snippets = { preset = 'luasnip' },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
   },
-  config = true,
-  opts = function()
-    local cmp = require('cmp')
-    local luasnip = require('luasnip')
-
-    local winhighlight = {
-      winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel',
-    }
-
-    local check_backspace = function()
-      local col = vim.fn.col('.') - 1
-      return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
-    end
-
-    return {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      -- I actually didn't realize this wasn't working for a while, and I'm kind of used
-      -- to using completion inside comments now.
-      -- I'll keep the code around for now
-      -- enabled = function()
-      --   local context = require('cmp.config.context')
-      --   -- keep command mode completion enabled when cursor is in a comment
-      --   if vim.api.nvim_get_mode().mode == 'c' then
-      --     return true
-      --   elseif disable_cmp_in[vim.bo.filetype] then
-      --     return false
-      --   else
-      --     -- disable completion inside a comment
-      --     local is_comment = context.in_treesitter_capture('comment') or context.in_syntax_group('Comment')
-      --     -- disable completion if inside a git commit or rebase
-      --     -- local buffer_name = vim.fn.expand('%:t')
-      --     -- local is_git_commit = buffer_name == 'COMMIT_EDITMSG' or buffer_name == 'git-rebase-todo'
-      --     -- return not is_comment and not is_git_commit
-      --     return not is_comment
-      --   end
-      -- end,
-      mapping = {
-        -- Navigate between suggestion items
-        ['<A-k>'] = cmp.mapping.select_prev_item(),
-        ['<A-j>'] = cmp.mapping.select_next_item(),
-        -- Scroll through docs
-        ['<C-k>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
-        ['<C-j>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
-        -- Manually open the the completions menu
-        ['<C-Space>'] = cmp.mapping(cmp.mapping.complete {}, { 'i', 'c' }),
-        -- I think this is usually bound to mapping.confirm({ select = false})
-        ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        -- Close the completions menu
-        ['<C-e>'] = cmp.mapping {
-          i = cmp.mapping.abort(),
-          c = cmp.mapping.close(),
-        },
-        -- Accept currently selected item. If none selected, `select` first item.
-        -- Set `select` to `false` to only confirm explicitly selected items.
-        ['<CR>'] = cmp.mapping.confirm { select = true },
-        -- Supertab
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expandable() then
-            luasnip.expand {}
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif check_backspace() then
-            fallback()
-          else
-            fallback()
-          end
-        end, {
-          'i',
-          's',
-        }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, {
-          'i',
-          's',
-        }),
-      },
-      formatting = {
-        fields = { 'kind', 'abbr', 'menu' },
-        format = function(entry, vim_item)
-          -- Kind icons
-          vim_item.kind = string.format('%s', kind_icons[vim_item.kind])
-          -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-          --
-          vim_item.menu = ({
-            nvim_lsp = '[LSP]',
-            nvim_lua = '[NvimLua]',
-            luasnip = '[Snippet]',
-            buffer = '[Buffer]',
-            path = '[Path]',
-          })[entry.source.name]
-
-          return vim_item
-        end,
-      },
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lua' },
-        { name = 'luasnip' },
-        { name = 'buffer' },
-        { name = 'path' },
-      },
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      },
-      window = {
-        completion = cmp.config.window.bordered(winhighlight),
-        documentation = cmp.config.window.bordered(winhighlight),
-      },
-      experimental = {
-        ghost_text = false,
-        native_menu = false,
-      },
-    }
-  end,
+  opts_extend = { 'sources.default' },
 }
