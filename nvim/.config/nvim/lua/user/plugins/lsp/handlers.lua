c36dbf9f5480ea6c52202311e4b4c7dd176c82a5
Adam Skoufis <adam.skoufis@gmail.com>
2025-05-08 21:28:12 +1000

    [nvim]: Remove deprecated API from diagnostic config

--- nvim/.config/nvim/lua/user/plugins/lsp/handlers.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/handlers.lua
@@ -1,38 +1,28 @@
 local M = {}
 
 M.setup = function()
-  local signs = {
-    { name = 'DiagnosticSignError', text = '' },
-    { name = 'DiagnosticSignWarn', text = '' },
-    { name = 'DiagnosticSignHint', text = '' },
-    { name = 'DiagnosticSignInfo', text = '' },
-  }
-
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
-  end
-
-  local config = {
-    -- disable virtual text
+  vim.diagnostic.config {
     virtual_text = false,
-    -- show signs
     signs = {
-      active = signs,
+      active = {
+        { name = 'DiagnosticSignError', text = '' },
+        { name = 'DiagnosticSignWarn', text = '' },
+        { name = 'DiagnosticSignHint', text = '' },
+        { name = 'DiagnosticSignInfo', text = '' },
+      },
     },
     update_in_insert = true,
     underline = true,
     severity_sort = true,
     float = {
-      focusable = false,
+      focusable = true,
       style = 'minimal',
       border = 'rounded',
-      source = 'always',
+      source = true,
       header = '',
       prefix = '',
     },
   }
-
-  vim.diagnostic.config(config)
 
   vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
     border = 'rounded',
