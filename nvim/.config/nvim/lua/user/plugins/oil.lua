45a2c2bb60717563bbe4cf6fec3033fc171d2bc5
Adam Skoufis <adam.skoufis@gmail.com>
2025-06-07 23:16:48 +1000

    [nvim]: Add hotkey for toggling file details inside `oil.nvim`

--- nvim/.config/nvim/lua/user/plugins/oil.lua
+++ nvim/.config/nvim/lua/user/plugins/oil.lua
@@ -1,3 +1,5 @@
+local detail = false
+
 return {
   'stevearc/oil.nvim',
   ---@module 'oil'
@@ -15,6 +17,20 @@ return {
       max_width = 0.8,
       max_height = 0.8,
       border = 'rounded',
+    },
+    keymaps = {
+      -- Copied from https://github.com/stevearc/oil.nvim/blob/08c2bce8b00fd780fb7999dbffdf7cd174e896fb/doc/recipes.md#toggle-file-detail-view
+      ['gd'] = {
+        desc = 'Toggle file detail view',
+        callback = function()
+          detail = not detail
+          if detail then
+            require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
+          else
+            require('oil').set_columns { 'icon' }
+          end
+        end,
+      },
     },
   },
   dependencies = { 'echasnovski/mini.icons' },
