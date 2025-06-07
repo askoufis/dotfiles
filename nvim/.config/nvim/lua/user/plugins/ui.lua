702965bcf3c806578b3a7938302c1049628f51e2
Adam Skoufis <adam.skoufis@gmail.com>
2025-06-07 16:30:05 +1000

    [nvim]: Remove unused `popup.nvim` plugin
    - I think this was a leftover from when I used to use telescope. AFAICT
      it's not used by anything any more, so I've removed it.

--- nvim/.config/nvim/lua/user/plugins/ui.lua
+++ nvim/.config/nvim/lua/user/plugins/ui.lua
@@ -5,9 +5,6 @@ return {
   -- component library
   { 'MunifTanjim/nui.nvim', lazy = true },
 
-  -- An implementation of the popup API from vim in neovim
-  { 'nvim-lua/popup.nvim', lazy = true },
-
   -- LSP progress inidicator
   { 'j-hui/fidget.nvim', version = '*', event = 'BufAdd', config = true },
 }
