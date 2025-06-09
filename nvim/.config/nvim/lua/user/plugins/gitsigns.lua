01115706fb0a567069797124b71c939d8c87afa0
Adam Skoufis <adam.skoufis@gmail.com>
2025-06-09 22:38:25 +1000

    [nvim]: Update `gitsigns` version and config
    - No need to specify highlights
    - Got rid of my custom signs, just using the defaults now
    - Updated keymaps as per docs
    - Updating to the latest version finally fixes the bug where changes to
      the status of a file outside of the editor weren't reflected in the
      editor

--- nvim/.config/nvim/lua/user/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/user/plugins/gitsigns.lua
@@ -1,28 +1,16 @@
 return {
   'lewis6991/gitsigns.nvim',
-  tag = 'v0.9.0',
+  version = '*',
   event = { 'BufReadPre', 'BufNewFile' },
   config = true,
   opts = function()
-    local gitsigns = require('gitsigns')
-
     return {
-      signs = {
-        add = { hl = 'GitSignsAdd', text = '▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-        change = { hl = 'GitSignsChange', text = '▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        delete = { hl = 'GitSignsDelete', text = '', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        topdelete = { hl = 'GitSignsDelete', text = '', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = {
-          hl = 'GitSignsChange',
-          text = '▎',
-          numhl = 'GitSignsChangeNr',
-          linehl = 'GitSignsChangeLn',
-        },
-      },
       attach_to_untracked = true,
       on_attach = function(bufnr)
-        local map = function(mode, l, r, opts)
-          opts = opts or { silent = true }
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
           opts.buffer = bufnr
           vim.keymap.set(mode, l, r, opts)
         end
@@ -30,44 +18,45 @@ return {
         -- Navigation
         map('n', ']c', function()
           if vim.wo.diff then
-            return ']c'
+            vim.cmd.normal { ']c', bang = true }
+          else
+            gitsigns.nav_hunk('next')
           end
-          vim.schedule(function()
-            gitsigns.next_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true })
+        end)
 
         map('n', '[c', function()
           if vim.wo.diff then
-            return '[c'
+            vim.cmd.normal { '[c', bang = true }
+          else
+            gitsigns.nav_hunk('prev')
           end
-          vim.schedule(function()
-            gitsigns.prev_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true })
+        end)
 
         -- Actions
-        map({ 'n', 'v' }, '<leader>hs', gitsigns.stage_hunk)
-        map({ 'n', 'v' }, '<leader>hr', gitsigns.reset_hunk)
+        -- Invoke again to unstage hunk
+        map('n', '<leader>hs', gitsigns.stage_hunk)
+        map('n', '<leader>hr', gitsigns.reset_hunk)
+
+        map('v', '<leader>hs', function()
+          gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') }
+        end)
+
+        map('v', '<leader>hr', function()
+          gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') }
+        end)
 
         map('n', '<leader>hS', gitsigns.stage_buffer)
-        map('n', '<leader>hu', gitsigns.undo_stage_hunk)
         map('n', '<leader>hR', gitsigns.reset_buffer)
+
         map('n', '<leader>hp', gitsigns.preview_hunk)
-        map('n', '<leader>hb', function()
-          gitsigns.blame_line { full = true }
-        end)
+
+        -- Toggles
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-        map('n', '<leader>hd', gitsigns.diffthis)
-        map('n', '<leader>hD', function()
-          gitsigns.diffthis('~')
-        end)
-        map('n', '<leader>td', gitsigns.toggle_deleted)
+        map('n', '<leader>tw', gitsigns.toggle_word_diff)
 
         -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        -- `vih`
+        map({ 'o', 'x' }, 'ih', gitsigns.select_hunk)
       end,
     }
   end,
