1263e7be460fcf4b8749b58faee8687d5817689d
Adam Skoufis <askoufis@seek.com.au>
2025-06-24 09:27:41 +1000

    [fish]: Tidy up `config.fish`

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,15 +3,17 @@ if status --is-interactive
     ssh_agent_init
 
     # stop Ctrl-s and Ctrl-q from screwing stuff up
-    stty -ixon # Commands to run in interactive sessions can go here
+    stty -ixon
+
+    # Commands to run in interactive sessions can go here
 
     # source the contents of the `private` directory
     for FILE in $HOME/.config/fish/private/*
         source $FILE
     end
 
-    # Source env first as other files depend on envars
     # --- Environment ---
+    # Source env first as other files depend on envars
     # Source base `env.fish` before OS-specific env files
     source $HOME/.config/fish/environment/env.fish
 
@@ -27,6 +29,9 @@ if status --is-interactive
 
     # --- Colorscheme
     source $HOME/.config/fish/colors/moonfly.fish
+
+    # --- Functions
+    # Functions are automatically loaded from the `functions` directory by fish
 
     # --- Mise
     ~/.local/bin/mise activate fish | source
