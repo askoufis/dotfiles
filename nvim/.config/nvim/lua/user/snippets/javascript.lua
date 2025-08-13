b06dd26967b295f7ca500d0c5b3aed85d9a8e8de
Adam Skoufis <adam.skoufis@gmail.com>
2025-08-13 19:41:04 +1000

    [nvim]: Make `clog` and `ins` snippets autosnippets

--- nvim/.config/nvim/lua/user/snippets/javascript.lua
+++ nvim/.config/nvim/lua/user/snippets/javascript.lua
@@ -5,6 +5,7 @@ return {
     {
       trig = 'clog',
       desc = 'Create a console.log statement and pass an object to it',
+      snippetType = 'autosnippet',
     },
     fmt('console.log({{ {} }})', {
       i(1),
@@ -15,6 +16,7 @@ return {
     {
       trig = 'ins',
       desc = 'Log an object with infinite depth and colours',
+      snippetType = 'autosnippet',
     },
     fmt('console.dir({{ {} }}, {{ colors: true, depth: null, maxArrayLength: null }})', {
       i(1),
