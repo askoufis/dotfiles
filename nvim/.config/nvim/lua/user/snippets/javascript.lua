978c8c747d6190653dc0619ca7aeef971fb7dfca
Adam Skoufis <askoufis@seek.com.au>
2025-08-14 13:56:49 +1000

    [nvim]: Mark `clog` and `ins` snippets as `hidden`
    - These snippets are in my muscle-memory. This config stops them from
      being suggested in the completion popup. Might experiment with making
      more of my snippets hidden in the future.

--- nvim/.config/nvim/lua/user/snippets/javascript.lua
+++ nvim/.config/nvim/lua/user/snippets/javascript.lua
@@ -6,6 +6,7 @@ return {
       trig = 'clog',
       desc = 'Create a console.log statement and pass an object to it',
       snippetType = 'autosnippet',
+      hidden = true,
     },
     fmt('console.log({{ {} }})', {
       i(1),
@@ -17,6 +18,7 @@ return {
       trig = 'ins',
       desc = 'Log an object with infinite depth and colours',
       snippetType = 'autosnippet',
+      hidden = true,
     },
     fmt('console.dir({{ {} }}, {{ colors: true, depth: null, maxArrayLength: null }})', {
       i(1),
