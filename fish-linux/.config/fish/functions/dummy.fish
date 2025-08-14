980f3d22b000ed8ff60932a749aaa68bf3402dc8
Adam Skoufis <adam.skoufis@gmail.com>
2025-08-14 22:44:20 +1000

    [fish-linux]: Add dummy linux-only `fish` function
    - This tricks stow into not symlinking `.config/fish/functions` so git
      doesn't track `fisher`-related files
    - The real way to fix this is to use `$fisher_path` as described
      here: https://github.com/jorgebucaran/fisher?tab=readme-ov-file#using-fisher_path-with-themes

---
+++ fish-linux/.config/fish/functions/dummy.fish
@@ -0,0 +1,5 @@
+function dummy
+    # Until I have a real linux-only fish function, this is a dummy function that will force stow to
+    # not symlink the entire `.config/fish/functions` directory, which means fisher and any fisher
+    # plugins don't end up tracked by git
+end
