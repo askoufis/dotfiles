5715739725a99b3b1103d928628063a803187bc4
Adam Skoufis <askoufis@seek.com.au>
2025-09-03 11:38:03 +1000

    [nvim]: Remove custom `ts_ls` config
    - Since https://github.com/neovim/nvim-lspconfig/pull/3955, the default
      `nvim-lspconfig` for `ts_ls` seems to handle monorepos much better, so
      I don't need a custom `root_dir` function any more.

--- nvim/.config/nvim/lua/user/plugins/lsp/servers/ts_ls.lua
+++ nvim/.config/nvim/lua/user/plugins/lsp/servers/ts_ls.lua
@@ -4,16 +4,4 @@ vim.lsp.config('ts_ls', {
     documentFormattingProvider = false,
     documentRangeFormattingProvider = false,
   },
-  settings = {
-    ts_ls = {},
-  },
-  root_dir = function(bufnr, cb)
-    local root =
-      vim.fs.root(bufnr, { 'pnpm-lock.yaml', 'pnpm-workspace.yaml', 'yarn.lock', 'package-lock.json', '.git' })
-    if root then
-      cb(root)
-    else
-      P('Failed to find a suitable root directory for ts_ls')
-    end
-  end,
 })
