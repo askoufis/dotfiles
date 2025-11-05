64a23d8e647ff124680e3990d4da2553e8943bcb
Adam Skoufis <askoufis@seek.com.au>
2025-11-05 13:41:15 +1100

    [fish-mac]: Add brew-installed `rustup` to `PATH`

--- fish-mac/.config/fish/environment/env-mac.fish
+++ fish-mac/.config/fish/environment/env-mac.fish
@@ -4,6 +4,8 @@ eval (/opt/homebrew/bin/brew shellenv)
 # Something is overriding $SHELL to be `/bin/sh`, so force it to be fish instead so tmux starts with fish
 set -x -g SHELL /opt/homebrew/bin/fish
 
+fish_add_path -pP (brew --prefix rustup)/bin
+
 # Increase the maximum number of open files and processes
 # https://gist.github.com/tombigel/d503800a282fcadbee14b537735d202c
 ulimit -n 200000
