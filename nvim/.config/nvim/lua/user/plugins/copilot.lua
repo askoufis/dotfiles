f33979c37e5242e916de995bfc025cd76043e324
Adam Skoufis <askoufis@seek.com.au>
2026-02-10 14:21:41 +1100

    [nvim]: Remove `copilot.vim` plugin

--- nvim/.config/nvim/lua/user/plugins/copilot.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'github/copilot.vim',
-  version = '*',
-  config = function()
-    set_keymap('i', '<C-l>', 'copilot#Accept("<CR>")', { expr = true, replace_keycodes = false })
-    vim.g.copilot_no_tab_map = true
-  end,
-  event = 'VeryLazy',
-}
