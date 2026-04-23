86d35b19adf6c2918965df81e9993f09a390a494
Adam Skoufis <adam.skoufis@gmail.com>
2026-04-23 20:10:47 +1000

    [fish]: Set telemetry opt-out envars

--- fish/.config/fish/environment/env.fish
+++ fish/.config/fish/environment/env.fish
@@ -31,3 +31,9 @@ set -x -g BAT_THEME "fly16"
 # Custom editor launch script so inspector plugins work with neovim
 # https://theosteiner.de/open-neovim-from-your-browser-integrating-nvim-with-sveltes-inspector
 set -x -g LAUNCH_EDITOR launch_editor_script
+
+# Opt out of telemetry in a standardized way. Bit of a dead standard though.
+set -x -g DO_NOT_TRACK true
+
+# Opt out of GitHub CLI telemetry
+set -x -g GH_TELEMETRY false
